340f17c10a8a1ebf8aa65a06b019291f1c4f51d1
Laszlo Budai <stentor.bgyk@gmail.com>
2016-02-05 18:11:49 +0100

    bootstrap.sh: simplified vagrantfile
    Removed unnecessary tools, etc...
    
    Signed-off-by: Laszlo Budai <stentor.bgyk@gmail.com>

--- Vagrantfile
+++ Vagrantfile
@@ -2,6 +2,5 @@ Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.box_url = "https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
   config.vm.provision :shell, :path => File.expand_path("../bootstrap.sh", __FILE__)
-  config.vm.provision :shell, :path =>  File.expand_path("../elasticsearch16_shield13.sh", __FILE__)
   config.vm.network "private_network", ip: "192.168.33.10"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,31 +1,29 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install expect -y
-sudo apt-get install openjdk-7-jre-headless -y
+apt-get install openjdk-7-jre-headless -y
 
 wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.3.deb
-sudo dpkg -i elasticsearch-1.7.3.deb
-
-# ES 2.1
-#sudo /usr/share/elasticsearch/bin/plugin -install license
-#sudo /usr/share/elasticsearch/bin/plugin -install shield
+dpkg -i elasticsearch-1.7.3.deb
 
 # ES 1.5, 1.7
 
-sudo /usr/share/elasticsearch/bin/plugin -install elasticsearch/license/latest
-sudo /usr/share/elasticsearch/bin/plugin -install elasticsearch/shield/latest
+/usr/share/elasticsearch/bin/plugin -install elasticsearch/license/latest
+/usr/share/elasticsearch/bin/plugin -install elasticsearch/shield/latest
 
-sudo echo "192.168.33.10 vagrant-es-server" >> /etc/hosts 
-sudo echo "192.168.33.1 vagrant-es-client" >> /etc/hosts
+/usr/share/elasticsearch/bin/shield/esusers useradd es_admin -r admin -p qqq123
 
-sudo echo "network.host: vagrant-es-server" >> /etc/elasticsearch/elasticsearch.yml
+echo "192.168.33.10 vagrant-es-server" >> /etc/hosts 
+echo "192.168.33.1 vagrant-es-client" >> /etc/hosts
 
-sudo echo "shield.ssl.keystore.path: /vagrant/certs/server/server.jks" >> /etc/elasticsearch/elasticsearch.yml
-sudo echo "shield.ssl.keystore.password:      qqq123" >> /etc/elasticsearch/elasticsearch.yml
-sudo echo "shield.transport.ssl:              true" >> /etc/elasticsearch/elasticsearch.yml
-sudo echo "shield.ssl.hostname.verification.resolve.name: false" >> /etc/elasticsearch/elasticsearch.yml
-sudo echo "transport.profiles.client.shield.ssl.client.auth: no" >> /etc/elasticsearch/elasticsearch.yml
-sudo echo "cluster.name: es-syslog-ng" >> /etc/elasticsearch/elasticsearch.yml
+echo "network.host: vagrant-es-server" >> /etc/elasticsearch/elasticsearch.yml
 
+echo "shield.ssl.keystore.path: /vagrant/certs/server/server.jks" >> /etc/elasticsearch/elasticsearch.yml
+echo "shield.ssl.keystore.password:      qqq123" >> /etc/elasticsearch/elasticsearch.yml
+echo "shield.transport.ssl:              true" >> /etc/elasticsearch/elasticsearch.yml
+echo "shield.ssl.hostname.verification.resolve.name: false" >> /etc/elasticsearch/elasticsearch.yml
+echo "transport.profiles.client.shield.ssl.client.auth: no" >> /etc/elasticsearch/elasticsearch.yml
+echo "cluster.name: es-syslog-ng" >> /etc/elasticsearch/elasticsearch.yml
+
+service elasticsearch start
